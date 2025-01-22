ce4324262520f5a1fb11921e4f16c7ddcd535a8d
Guardianprime <okongordian@gmail.com>
2025-01-22 12:04:08 +0100

    hover styles and basic javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll("#nav .nav-links");
+const cuurentTimeEl = document.getElementById("current");
+const previousTimeEl = document.getElementById("previous");
+
+
+navLinks.addEventListener("click", handleClick);
+
+function handleClicks() {
+console.log("working");
+}
