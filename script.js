0e665d8c53ec315e1e2f72114720d0f30ebc8c1a
Gordian okon <102440179+guardianprime@users.noreply.github.com>
2025-03-05 00:39:26 +0000

    minor changes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentElements = document.querySelectorAll(".current");
     const previousElements = document.querySelectorAll(".previous");
 
-
-
     navLinks.forEach((link) => {
         link.addEventListener("click", async (e) => {
             // Remove "active" class from all list items
@@ -40,12 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
     async function getData(number) {
         const res = await fetch(`./data.json`);
         if (!res.ok) {
-        throw new Error(`Failed to fetch data: ${res.status}`);
-      }
+            throw new Error(`Failed to fetch data: ${res.status}`);
+        }
         const data = await res.json();
         return data[number];
     }
-
-
-
 });
